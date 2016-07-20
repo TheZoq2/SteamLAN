20fa1b306e985cc822365b0436c2f7937f0bdfa2
TheZoq2 <frans.skarman@gmail.com>
2016-07-20 23:12:41 +0200

    Add script

---
+++ findFriendGames.py
@@ -0,0 +1,54 @@
+import urllib.request;
+import json;
+
+
+def getUserGames(userID):
+    requestString = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={}&steamid={}&format=json".format(apiKey, userID);
+    
+    lines = urllib.request.urlopen(requestString) 
+    result = ""
+    for line in lines:
+        result += line.decode("utf-8");
+
+    return result
+
+
+
+def getUserGameList(userID):
+    gameString = getUserGames(userID)
+
+    data = json.loads(gameString)
+
+    gameList = data["response"]["games"];
+
+    result = []
+    for game in gameList:
+        result.append(game["appid"])
+
+    return result
+
+def getCommonGames(userList):
+    commonGames = {}
+    
+    for user in userList:
+        userGames = getUserGameList(user)
+
+
+        for game in userGames:
+            if not game in commonGames:
+                commonGames[game] = 0
+            
+            commonGames[game] += 1
+
+    result = []
+    for game in commonGames:
+        if commonGames[game] == len(userList):
+            result.append(game)
+
+    return result
+
+user1 = "76561197996901884"
+user2 = "76561198016701707"
+
+#getUserGamesJson(userID);
+print(getCommonGames([user1, user2, "76561198014865373", "76561198010718921"]))
